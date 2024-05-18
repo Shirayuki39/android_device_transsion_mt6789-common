41482cd7a54f2f051d296a09e1b8422822c69fa4
Shirayuki39 <lorddemecrius83@proton.me>
2024-05-18 16:30:44 +0800

    Revert "transsion-mt6789: temporarily uncommonize so actions won't, I don't need it anymore
    This reverts commit 481619acd34873d04b175a6a70c66b42e4c0fcc1.

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -24,13 +24,27 @@ if [ ! -f "${HELPER}" ]; then
 fi
 source "${HELPER}"
 
-# Initialize the helper
-setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}"
+# Initialize the helper for common
+setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true
 
 # Warning headers and guards
-write_headers
+write_headers "TECNO-LG8n TECNO-LG8n"
 
 write_makefiles "${MY_DIR}/proprietary-files.txt" true
 
 # Finish
 write_footers
+
+if [ -s "${MY_DIR}/../${DEVICE}/proprietary-files.txt" ]; then
+    # Reinitialize the helper for device
+    setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false
+
+    # Warning headers and guards
+    write_headers
+
+    # The standard device blobs
+    write_makefiles "${MY_DIR}/../${DEVICE}/proprietary-files.txt" true
+
+    # Finish
+    write_footers
+fi
